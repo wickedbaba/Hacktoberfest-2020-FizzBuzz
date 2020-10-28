01d9d1158f5ff7f3d8353f77a988cf4afcd8df08
wickedbaba <54187786+wickedbaba@users.noreply.github.com>
2020-10-28 23:49:04 +0530

    Update fizzbuzz_recursive.cpp

--- cpp/fizzbuzz_recursive.cpp
+++ cpp/fizzbuzz_recursive.cpp
@@ -21,3 +21,50 @@ int main(){
     fizz(1);
     return 0;
 }
+
+void MergeSortedIntervals(vector<int>& v, int s, int m, int e) {
+
+
+	vector<int> temp;
+
+	int i, j;
+	i = s;
+	j = m + 1;
+
+	while (i <= m && j <= e) {
+
+		if (v[i] <= v[j]) {
+			temp.push_back(v[i]);
+			++i;
+		}
+		else {
+			temp.push_back(v[j]);
+			++j;
+		}
+
+	}
+
+	while (i <= m) {
+		temp.push_back(v[i]);
+		++i;
+	}
+
+	while (j <= e) {
+		temp.push_back(v[j]);
+		++j;
+	}
+
+	for (int i = s; i <= e; ++i)
+		v[i] = temp[i - s];
+
+}
+
+
+void MergeSort(vector<int>& v, int s, int e) {
+	if (s < e) {
+		int m = (s + e) / 2;
+		MergeSort(v, s, m);
+		MergeSort(v, m + 1, e);
+		MergeSortedIntervals(v, s, m, e);
+	}
+}
